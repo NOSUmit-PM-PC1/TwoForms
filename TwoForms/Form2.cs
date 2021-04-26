c635445b40586af21dbb0b782066d36ed5ac812a
Мария <mahadrum@yandex.ru>
2021-04-26 11:43:40 +0300

    Добавьте файлы проекта.

---
+++ TwoForms/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoForms
+{
+    public partial class Form2 : Form
+    {
+        public string stroka;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = stroka;
+        }
+    }
+}
